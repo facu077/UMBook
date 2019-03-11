5585d938eda5d06a0ff205cf379f9e490e50f7a6
Facundo <facu.m077@gmail.com>
2019-03-10 23:52:48 -0300

    Implemented 'Ver Mi Muro' and 'Send Messages'

--- 4-Implementacion/src/main/webapp/app/entities/mensaje/mensaje.component.ts
+++ 4-Implementacion/src/main/webapp/app/entities/mensaje/mensaje.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import * as moment from 'moment';
 
-import { IMensaje } from 'app/shared/model/mensaje.model';
-import { AccountService } from 'app/core';
+import { IMensaje } from '../../shared/model/mensaje.model';
+import { IMuro } from '../../shared/model/muro.model';
+import { AccountService } from '../../core';
 import { MensajeService } from './mensaje.service';
+import { DATE_TIME_FORMAT } from '../../shared/constants/input.constants';
 
 @Component({
     selector: 'jhi-mensaje',
     templateUrl: './mensaje.component.html'
 })
 export class MensajeComponent implements OnInit, OnDestroy {
+    @Input() muro: IMuro;
+
     mensajes: IMensaje[];
+    mensajesMuro: IMensaje[] = [];
     currentAccount: any;
     eventSubscriber: Subscription;
-    currentSearch: string;
+    currentTxt: string;
+
+    isSaving: boolean;
 
     constructor(
         protected mensajeService: MensajeService,
@@ -25,26 +33,9 @@ export class MensajeComponent implements OnInit, OnDestroy {
         protected eventManager: JhiEventManager,
         protected activatedRoute: ActivatedRoute,
         protected accountService: AccountService
-    ) {
-        this.currentSearch =
-            this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search']
-                ? this.activatedRoute.snapshot.params['search']
-                : '';
-    }
+    ) {}
 
     loadAll() {
-        if (this.currentSearch) {
-            this.mensajeService
-                .search({
-                    query: this.currentSearch
-                })
-                .pipe(
-                    filter((res: HttpResponse<IMensaje[]>) => res.ok),
-                    map((res: HttpResponse<IMensaje[]>) => res.body)
-                )
-                .subscribe((res: IMensaje[]) => (this.mensajes = res), (res: HttpErrorResponse) => this.onError(res.message));
-            return;
-        }
         this.mensajeService
             .query()
             .pipe(
@@ -54,22 +45,47 @@ export class MensajeComponent implements OnInit, OnDestroy {
             .subscribe(
                 (res: IMensaje[]) => {
                     this.mensajes = res;
-                    this.currentSearch = '';
+                    if (this.mensajes && this.mensajes.length > 0) {
+                        for (const msj of this.mensajes) {
+                            if (msj.muro.id === this.muro.id) {
+                                this.mensajesMuro.push(msj);
+                            }
+                        }
+                    }
                 },
                 (res: HttpErrorResponse) => this.onError(res.message)
             );
     }
 
-    search(query) {
-        if (!query) {
-            return this.clear();
-        }
-        this.currentSearch = query;
+    newMsj() {
+        this.isSaving = true;
+        const newMensaje: IMensaje = {
+            fecha: moment(new Date(), DATE_TIME_FORMAT),
+            texto: this.currentTxt,
+            muro: this.muro
+        };
+        this.subscribeToSaveResponse(this.mensajeService.create(newMensaje));
+        this.currentTxt = '';
+    }
+
+    canSendMsj(): boolean {
+        return this.currentTxt && this.currentTxt !== '';
+    }
+
+    protected subscribeToSaveResponse(result: Observable<HttpResponse<IMensaje>>) {
+        result.subscribe((res: HttpResponse<IMensaje>) => this.onSaveSuccess(), (res: HttpErrorResponse) => this.onSaveError());
+    }
+
+    protected onSaveSuccess() {
+        this.isSaving = false;
         this.loadAll();
     }
 
+    protected onSaveError() {
+        this.isSaving = false;
+    }
+
     clear() {
-        this.currentSearch = '';
         this.loadAll();
     }
 
